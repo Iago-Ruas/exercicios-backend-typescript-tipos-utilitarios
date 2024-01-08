54b88e9cd1509eb4d340f8274aa052ef46e99496
Iago-Ruas <vtzard@gmail.com>
2024-01-08 20:07:45 -0300

    feat: answer question 1

--- 01/scripts.ts
+++ 01/scripts.ts
@@ -1,11 +1,17 @@
 type Usuario = {
-    nome: string,
-    email: string,
-    cpf: string,
-    rg: string
-    dataNacimento?: string
-}
+    nome: string;
+    email: string;
+    cpf: string;
+    rg: string;
+    dataNacimento?: string;
+};
 
 const cadastrarUsuário = (info: Usuario): Usuario => {
     return info;
-}
+};
+
+type UsuarioSemRg = Omit<Usuario, "rg">;
+
+const futuraImplementação = (info: UsuarioSemRg) => {
+    return info;
+};
